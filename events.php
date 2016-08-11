739e37354da90ad72bceab79b145547de17a6d08
jgravesjr <jgravesjr@outlook.com>
2016-08-10 22:15:54 -0400

    Added initial template and scaffolding.
    Added components: bootstrap.min.css bootstrap.min.js bootstrap-theme.min.css
    npm.js jquery.min.js.
    
    Added scaffolding to all pages, in php for the moment, but once the content
    is placed will be transfered into javascript where possible.

---
+++ events.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hellraisin?</title>
+		<!--Get data from viewport global, set initial scale of data-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!--Add basic bootstrap css-->
+		<link href='_/css/bootstrap.min.css' rel='stylesheet' media='screen'>
+		<!--Bootstrap theme css-->
+		<link href='_/css/bootstrap-theme.min.css' rel='stylesheet' media='screen'>
+		<!--Local stylesheet-->
+		<link href='_/css/localStyles.min.css' rel='stylesheet' media='screen'>
+	</head>
+	<body id="events">
+		<section class="container">
+			<div class="content row">
+				<section class="main col col-lg-8">
+				</section><!--Main Bar-->
+				<section class="sidebar col col-4">
+				</section><!--Side Bar-->
+			</div>
+		</section><!--Container-->		
+
+		<!--Loaded at the bottom to help performance@mdo :)-->
+		<!--Bootstrap js and jQuery dependency-->
+		<script src='_/js/bootstrap.min.js'></script>
+		<script src='_/js/jquery.min.js'></script>
+		<!--Local js functions-->
+		<script src='_/js/localScripts.min.js'></script>
+	</body>
+</html>
